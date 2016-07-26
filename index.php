1c4053ab8b2926de3cb00b1c97e2d808a79e9cf6
Michael Ray <mray190@gmail.com>
2016-07-26 12:52:42 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Macro:Bytes</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="images/favicon.png">
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Intro</a></li>
+							<li><a href="#team">Meet the team</a></li>
+							<li><a href="#challenge">The challenge</a></li>
+							<li><a href="#submission">Our submission</a></li>
+							<li><a href="#social">#ARMINTERNS</a></li>
+							<li><a href="#contact">Contact Us</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+				<section id="intro" class="wrapper style1 fullscreen fade-up">
+					<div class="inner">
+						<h1>Macro:Bytes</h1>
+						<p>An entry to a summer ARM intern competition centered around micro:bits</p>
+						<ul class="actions">
+							<li><a href="#team" class="button scrolly">Get Started!</a></li>
+						</ul>
+					</div>
+				</section>
+
+				<!-- team -->
+				<section id="team" class="wrapper style2 spotlights">
+					<section>
+						<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+						<div class="content">
+							<div class="inner">
+								<h2>Sam Schwebach</h2>
+								Masters student at the University of Wisconsin 
+							</div>
+						</div>
+					</section>
+					<section>
+						<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+						<div class="content">
+							<div class="inner">
+								<h2>Michael Ray</h2>
+								Undergraduate student at the University of Michigan
+								<!-- <ul class="actions">
+									<li><a href="#" class="button">Login</a></li>
+								</ul> -->
+							</div>
+						</div>
+					</section>
+					<section>
+						<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="top center" /></a>
+						<div class="content">
+							<div class="inner">
+								<h2>Ryan Corey</h2>
+								Ph.D student at the University of Illinois Urbana-Champaign
+								<!-- <ul class="actions">
+									<li><a href="#" class="button">Login</a></li>
+								</ul> -->
+							</div>
+						</div>
+					</section>
+				</section>
+
+				<!-- challenge -->
+				<section id="challenge" class="wrapper style3 fade-up">
+					<div class="inner">
+						<h2>The Challenge</h2>
+					</div>
+				</section>
+
+				<!-- submission -->
+				<section id="submission" class="wrapper style1 fade-up">
+					<div class="inner">
+						<h2>Our Submission</h2>
+						<div class="features">
+							<section>
+								<a href="generic.html">
+								<span class="icon major fa-chain"></span>
+								<h3>Brainstorming</h3>
+								How we came up with our idea and details regarding our choice
+								</a>
+							</section>
+							<section>
+								<a href="generic.html">
+								<span class="icon major fa-cog"></span>
+								<h3>Technical Details</h3>
+								How to setup the micro:bit, and what hardware you will need
+								</a>
+							</section>
+							<section>
+								<a href="generic.html">
+								<span class="icon major fa-code"></span>
+								<h3>Coding</h3>
+								How to code the micro:bit!
+								</a>
+							</section>
+							<section>
+								<a href="generic.html">
+								<span class="icon major fa-desktop"></span>
+								<h3>Android App</h3>
+								Download our android app to connect to your micro:bit and run the code!
+								</a>
+							</section>
+							<section>
+								<a href="generic.html">
+								<span class="icon major fa-lock"></span>
+								<h3>Final words</h3>
+								After completing our project, this is what we had to say
+								</a>
+							</section>
+							<section>
+								<a href="generic.html">
+								<span class="icon major fa-diamond"></span>
+								<h3>Submission details</h3>
+								
+								</a>
+							</section>
+						</div>
+					</div>
+				</section>
+
+				<!-- social -->
+					<section id="social" class="wrapper style2 fade-up">
+						<div class="inner">
+							<h2>#ARMINTERNS</h2>
+
+							<div class="features">
+								<section>
+									Testing testing 123
+								</section>
+								<section>
+									<a class="twitter-timeline"  href="https://twitter.com/hashtag/arminterns" data-widget-id="753241964712034304">#arminterns Tweets</a>
+					            	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);} var e = document.getElementById('twitter-widget-0'); e.style = 'width: 100%;'}(document,"script","twitter-wjs");;</script>
+								</section>
+								<section>
+									<div id="embedded-feed" style="height:800px;width:100%;"></div>
+									<script type="text/javascript" src="https://s0.assets-yammer.com/assets/platform_embed.js"></script>
+									<script type="text/javascript">
+										yam.connect.embedFeed({
+										  container: "#embedded-feed",
+										  network: "arm.com",
+										  feedType: "group",
+										  feedId: "8042355"
+										});
+									</script>
+								</section>
+							</div>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="contact" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Have a question for us?</h2>
+							<!-- <p>Kyle and Laura aren't allowed to comment.</p> -->
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="field half first">
+											<label for="name">Name</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="email">Email</label>
+											<input type="text" name="email" id="email" />
+										</div>
+										<div class="field">
+											<label for="message">Message</label>
+											<textarea name="message" id="message" rows="5"></textarea>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Send Message</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<span>5707 Southwest Pkwy<br />
+											Bldg 1 Ste 100<br />
+											Austin, TX<br />
+											78735</span>
+										</li>
+										<li>
+											<h3>Email</h3>
+											<a href="#">michael.ray@arm.com</a>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<span>(512) 327-9249</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
+												<li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
+												<li><a href="#" class="fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="#" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						Designed by Michael Ray
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
